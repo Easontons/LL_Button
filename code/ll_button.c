ac4d1d75e393577c77605f78ccf10d7f842d0a0b
eason <eason_mo@outlook.com>
2023-08-12 23:20:07 +0800

    this is first commit, but no example.

---
+++ code/ll_button.c
@@ -0,0 +1,373 @@
+#include "ll_button.h"
+
+/* ll button head */
+static ll_btn_obj_t ll_btn_head = {0};
+
+/* Configure ll button */
+static void ll_btn_conf(ll_btn_obj_t *btn,
+                        const char   *name,
+                        uint32_t      active_level,
+                        uint32_t      debounce_term,
+                        uint32_t      debounce_again,
+                        uint32_t      long_press_term,
+                        uint32_t      tapping_term)
+{
+    memset(btn, 0, sizeof(ll_btn_obj_t));
+    btn->name = name;
+    btn->active_level = active_level;
+    btn->debounce_term = debounce_term;
+    btn->debounce_again = debounce_again;
+    btn->long_press_term = long_press_term;
+    btn->tapping_term = tapping_term;
+    btn->state = LL_BTN_INIT;
+    /**
+     *  Set button level to deactive level
+     *  Explain:
+     *  If button active level is 0, and button default level(btn_level) is 0 in initialization,
+     *  than button will go to active state, but this is not expected,
+     *  so set button level to deactive level in conf() to avoid this happening.
+     */
+    btn->btn_level = 1 - btn->active_level;
+}
+
+/* Insert a ll button to linked list */
+static void ll_btn_insert(ll_btn_obj_t *btn)
+{
+    ll_btn_obj_t *target = &ll_btn_head;
+    uint16_t idx = 0;
+    while (target->next != NULL)
+    {
+        target = target->next;
+        idx++;
+    }
+    target->next = btn;
+    btn->idx = idx;
+}
+
+void ll_btn_dynamic_creat_simple(const char   *name,
+                                 uint32_t      active_level)
+{
+    ll_btn_obj_t *btn = (ll_btn_obj_t *)malloc(sizeof(ll_btn_obj_t));
+    if (btn == NULL)
+    {
+        return;
+    }
+
+    ll_btn_conf(btn,
+                name,
+                active_level,
+                LL_BTN_DEFAULT_DEBOUNCE_TERM,
+                LL_BTN_DEFAULT_DEBOUNCE_AGAIN,
+                LL_BTN_DEFAULT_LONG_PRESS_TERM,
+                LL_BTN_DEFAULT_TAPPING_TERM
+                );
+    ll_btn_insert(btn);
+}
+
+void ll_btn_dynamic_creat(const char  *name,
+                          uint32_t     active_level,
+                          uint32_t     debounce_term,
+                          uint32_t     debounce_again,
+                          uint32_t     long_press_term,
+                          uint32_t     tapping_term)
+{
+    ll_btn_obj_t *btn = (ll_btn_obj_t *)malloc(sizeof(ll_btn_obj_t));
+    if (btn == NULL)
+    {
+        return;
+    }
+
+    ll_btn_conf(btn,
+                name,
+                active_level,
+                debounce_term,
+                debounce_again,
+                long_press_term,
+                tapping_term);
+    ll_btn_insert(btn);
+}
+
+void ll_btn_static_creat_simple(ll_btn_obj_t *btn,
+                                const char   *name,
+                                uint32_t      active_level)
+{
+    ll_btn_conf(btn,
+                name,
+                active_level,
+                LL_BTN_DEFAULT_DEBOUNCE_TERM,
+                LL_BTN_DEFAULT_DEBOUNCE_AGAIN,
+                LL_BTN_DEFAULT_LONG_PRESS_TERM,
+                LL_BTN_DEFAULT_TAPPING_TERM
+                );
+    ll_btn_insert(btn);
+}
+
+void ll_btn_static_creat(ll_btn_obj_t *btn,
+                          const char  *name,
+                          uint32_t     active_level,
+                          uint32_t     debounce_term,
+                          uint32_t     debounce_again,
+                          uint32_t     long_press_term,
+                          uint32_t     tapping_term)
+{
+    ll_btn_conf(btn,
+                name,
+                active_level,
+                debounce_term,
+                debounce_again,
+                long_press_term,
+                tapping_term);
+    ll_btn_insert(btn);
+}
+
+/* Weak function, need give a same name function in your project,
+ * and this functon need provide every button level by button index */
+__weak uint32_t ll_btn_level_get(ll_btn_obj_t *btn)
+{
+    return 0;
+}
+
+/* Print all ll buttons */
+void ll_btn_print_list(void)
+{
+    ll_btn_obj_t *btn = &ll_btn_head;
+    while (btn->next != NULL)
+    {
+        btn = btn->next;
+        LL_BTN_DEBUG_NAME("\r\n");
+    }
+}
+
+/* Reset button to initial state */
+static void ll_btn_reset(ll_btn_obj_t *btn)
+{
+    btn->state = LL_BTN_INIT;
+    btn->tick = 0;
+}
+
+void ll_btn_attach_by_idx(uint32_t idx, uint8_t cb_type, ll_btn_ops_cb cb)
+{
+    ll_btn_obj_t *btn = ll_btn_head.next;
+    uint32_t count = idx;
+    while (count > 0)
+    {
+        btn = btn->next;
+        count--;
+    }
+    if (cb_type >= LL_BTN_CB_IDX_END)
+    {
+        LL_BTN_DEBUG("this type callback function does not preinstall.\r\n");
+        return;
+    }
+    btn->cb[cb_type] = cb;
+    if (btn->cb[cb_type] == NULL)
+    {
+        LL_BTN_DEBUG("callback function idx %d attach NULL.\r\n", cb_type);
+    }
+    else
+    {
+        LL_BTN_DEBUG("callback function idx %d attach success.\r\n", cb_type);
+    }
+}
+void ll_btn_detach_by_idx(uint32_t idx, uint8_t cb_type)
+{
+    ll_btn_obj_t *btn = ll_btn_head.next;
+    uint32_t count = idx;
+    while (count > 0)
+    {
+        btn = btn->next;
+        count--;
+    }
+    if (cb_type >= LL_BTN_CB_IDX_END)
+    {
+        LL_BTN_DEBUG("this type callback function do not preinstall.\r\n");
+        return;
+    }
+    btn->cb[cb_type] = NULL;
+}
+
+void ll_btn_attach_by_obj(ll_btn_obj_t *btn, uint8_t cb_type, ll_btn_ops_cb cb)
+{
+    if (cb_type >= LL_BTN_CB_IDX_END)
+    {
+        LL_BTN_DEBUG("this type callback function does not preinstall.\r\n");
+        return;
+    }
+    btn->cb[cb_type] = cb;
+    if (btn->cb[cb_type] == NULL)
+    {
+        LL_BTN_DEBUG("callback function idx %d attach NULL.\r\n", cb_type);
+    }
+    else
+    {
+        LL_BTN_DEBUG("callback function idx %d attach success.\r\n", cb_type);
+    }
+}
+
+void ll_btn_detach_by_obj(ll_btn_obj_t *btn, uint8_t cb_type)
+{
+    if (cb_type >= LL_BTN_CB_IDX_END)
+    {
+        LL_BTN_DEBUG("this type callback function do not preinstall.\r\n");
+        return;
+    }
+    btn->cb[cb_type] = NULL;
+}
+
+void ll_btn_debounce(ll_btn_obj_t *btn, uint32_t curr_tick, uint32_t curr_level)
+{
+    uint32_t tick_diff;
+    /* no need debounce or debounce method external */
+    if (btn->debounce_term == 0)
+    {
+        btn->btn_level = curr_level;
+        return;
+    }
+
+    if (curr_level == btn->btn_level)
+    {
+        btn->is_debouncing = LL_BTN_FALSE;
+    }
+    else if ((curr_level != btn->btn_level) && (btn->is_debouncing == LL_BTN_FALSE))
+    {
+        btn->is_debouncing = LL_BTN_TRUE;
+        btn->tick = curr_tick;
+        LL_BTN_DEBUG("debounce start, tick is %ld.\r\n", btn->tick);
+    }
+
+    if (btn->is_debouncing == LL_BTN_TRUE)
+    {
+        tick_diff = LL_BTN_TICK_DIFF(curr_tick, btn->tick);
+        if (tick_diff >= btn->debounce_term)
+        {
+            if (btn->debounce_cnt >= btn->debounce_again)
+            {
+                btn->is_debouncing = LL_BTN_FALSE;
+                btn->btn_level = curr_level;
+                btn->tick = curr_tick;
+                btn->debounce_cnt = 0;
+                LL_BTN_DEBUG("debounce end, tick is %ld.\r\n", btn->tick);
+            }
+            else
+            {
+                btn->tick = curr_tick;
+                btn->debounce_cnt++;
+            }
+        }
+    }
+}
+
+/**
+ * @brief ll button finite state machine
+ * @param btn ll button object
+ * @param curr_tick current tick
+ */
+void ll_btn_fsm(ll_btn_obj_t *btn, uint32_t curr_tick)
+{
+    uint32_t tick_diff = LL_BTN_TICK_DIFF(curr_tick, btn->tick);
+    switch (btn->state)
+    {
+    case LL_BTN_INIT:
+        if (btn->btn_level == btn->active_level)
+        {
+            btn->state = LL_BTN_DOWN;
+        }
+        break;
+    case LL_BTN_DOWN:
+        if (btn->cb[LL_BTN_DOWN_CB])
+        {
+            btn->cb[LL_BTN_DOWN_CB](btn);
+        }
+        btn->state = LL_BTN_PRESSED;
+        btn->tick = curr_tick;
+        LL_BTN_DEBUG("button down, tick is %lu.\r\n", btn->tick);
+        break;
+    case LL_BTN_PRESSED:
+        if (btn->btn_level != btn->active_level)
+        {
+            if (tick_diff < btn->tapping_term)
+            {
+                btn->state = LL_BTN_UP;
+            }
+            else
+            {
+                btn->state = LL_BTN_RELEASED;
+            }
+            btn->tick = curr_tick;
+        }
+        else if ((btn->btn_level == btn->active_level) && (tick_diff > btn->long_press_term))
+        {
+            if (btn->cb[LL_BTN_LONG_PRESS_START_CB])
+            {
+                btn->cb[LL_BTN_LONG_PRESS_START_CB](btn);
+            }
+            /* if long press trigger, tap dance cnt need clear */
+            btn->tapping_cnt = 0;
+            btn->state = LL_BTN_PRESSEND;
+            btn->tick = curr_tick;
+            LL_BTN_DEBUG("button long press start, tick is %lu.\r\n", btn->tick);
+        }
+        break;
+    case LL_BTN_PRESSEND:
+        if (btn->btn_level == btn->active_level)
+        {
+            if (btn->cb[LL_BTN_LONG_PRESS_DURING_CB])
+            {
+                btn->cb[LL_BTN_LONG_PRESS_DURING_CB](btn);
+            }
+        }
+        else if (btn->btn_level != btn->active_level)
+        {
+            if (btn->cb[LL_BTN_LONG_PRESS_STOP_CB])
+            {
+                btn->cb[LL_BTN_LONG_PRESS_STOP_CB](btn);
+            }
+            LL_BTN_DEBUG("button long press stop, tick is %lu.\r\n", btn->tick);
+            btn->state = LL_BTN_RELEASED;
+            btn->tick = curr_tick;
+        }
+        break;
+    case LL_BTN_UP:
+        btn->tapping_cnt++;
+        if (btn->cb[LL_BTN_UP_CB])
+        {
+            btn->cb[LL_BTN_UP_CB](btn);
+        }
+        LL_BTN_DEBUG("button up, tick is %lu.\r\n", btn->tick);
+        btn->state = LL_BTN_RELEASED;
+        btn->tick = curr_tick;
+        break;
+    case LL_BTN_RELEASED:
+        if (btn->btn_level == btn->active_level)
+        {
+            btn->state = LL_BTN_DOWN;
+        }
+        else if (tick_diff > btn->tapping_term)
+        {
+            if (btn->cb[LL_BTN_TAP_CB] && (btn->tapping_cnt > 0))
+            {
+                btn->cb[LL_BTN_TAP_CB](btn);
+                LL_BTN_DEBUG("button tap, tap cnt is %d.\r\n", btn->tapping_cnt);
+                btn->tapping_cnt = 0;
+            }
+            LL_BTN_DEBUG("button released, tick is %lu.\r\n", btn->tick);
+            ll_btn_reset(btn);
+        }
+        break;
+    default:
+        break;
+    }
+}
+
+void ll_btn_tick(uint32_t curr_tick)
+{
+    ll_btn_obj_t *target = &ll_btn_head;
+    while (target->next != NULL)
+    {
+        target = target->next;
+        uint32_t level = ll_btn_level_get(target);
+        ll_btn_debounce(target, curr_tick, level);
+        ll_btn_fsm(target, curr_tick);
+    }
+}
+
---
+++ code/ll_button.h
@@ -0,0 +1,211 @@
+#ifndef LL_BUTTON_H
+#define LL_BUTTON_H
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+#ifndef __weak
+#define __weak __attribute__((__weak__))
+#endif
+
+/* ll button operations callback function */
+typedef void (*ll_btn_ops_cb)(void*);
+/* ll button level get callback function */
+typedef uint32_t (*ll_btn_level_get_cb)(void*);
+
+/* button state */
+typedef enum
+{
+    LL_BTN_INIT = 0,
+    LL_BTN_DOWN,
+    LL_BTN_PRESSED,
+    LL_BTN_PRESSEND,
+    LL_BTN_UP,
+    LL_BTN_RELEASED,
+} ll_btn_state;
+
+/* button state callback function preinstall, add custom function you need above *LL_BTN_IDX_END* */
+typedef enum
+{
+    LL_BTN_DOWN_CB = 0,
+    LL_BTN_UP_CB,
+    LL_BTN_LONG_PRESS_START_CB,
+    LL_BTN_LONG_PRESS_DURING_CB,
+    LL_BTN_LONG_PRESS_STOP_CB,
+    LL_BTN_TAP_CB,
+    LL_BTN_CB_IDX_END
+} ll_btn_cb_type;
+
+/* ll button object */
+typedef struct LL_BTN_OBJ
+{
+    volatile uint32_t    tick;
+    uint32_t             active_level : 1;
+    volatile uint32_t    btn_level : 1;
+    uint32_t             state : 3;
+    uint32_t             debounce_term : 8;
+    uint32_t             debounce_again : 4;
+    uint32_t             debounce_cnt : 4;
+    uint32_t             is_debouncing : 1;
+    uint32_t             idx : 10;
+    uint32_t             long_press_term : 16;
+    uint32_t             tapping_term : 10;
+    uint32_t             tapping_cnt : 6;
+    const char          *name;
+    ll_btn_ops_cb        cb[LL_BTN_CB_IDX_END];
+    struct LL_BTN_OBJ   *next;
+} ll_btn_obj_t;
+
+
+#define LL_BTN_TRUE  1
+#define LL_BTN_FALSE 0
+
+/* Using debug set 1, no debug set 0 */
+#define LL_BTN_DEBUG_ON 0
+#define LL_BTN_DEBUG_NAME(__info, ...) do { \
+                                          printf("LL_BTN_DEBUG(idx:%d, name:%s)::", btn->idx, btn->name); \
+                                          printf(__info, ##__VA_ARGS__); \
+                                        } while(0)
+#if LL_BTN_DEBUG_ON
+#define LL_BTN_DEBUG(__info, ...) LL_BTN_DEBUG_NAME(__info, ##__VA_ARGS__)
+#else
+#define LL_BTN_DEBUG(__info, ...)
+#endif
+
+/* Get 2 tick difference(32-bit) */
+#define LL_BTN_TICK_DIFF_32(curr, last) ((curr) > (last) ? (uint32_t)((curr) - (last)) : (UINT32_MAX) - (last) + (curr) + 1)
+#define LL_BTN_TICK_DIFF(curr, last)  LL_BTN_TICK_DIFF_32(curr, last)
+
+/* ================================= TIPS ================================== */
+/* 1.TIME RULE
+ * debouce_time < tap_dance_interval_time << long_press_interval_time
+ */
+
+/* Default setting for using, time unit: ms */
+/* default active level */
+#define LL_BTN_DEFAULT_ACTIVE_LEVEL 1
+/* debounce term */
+#define LL_BTN_DEFAULT_DEBOUNCE_TERM 5
+/* do debounce again times */
+#define LL_BTN_DEFAULT_DEBOUNCE_AGAIN 0
+/* max 65535ms */
+#define LL_BTN_DEFAULT_LONG_PRESS_TERM 500
+/* 70 ~ 120ms better */
+#define LL_BTN_DEFAULT_TAPPING_TERM 100
+
+/* ========================== LL BUTTON FUNCTIONS ========================== */
+/**
+ * @brief Dynamic creat a ll button, only need set 2 params, other is default
+ *
+ * @param name          Button name
+ * @param active_level  Button active level
+ */
+void ll_btn_dynamic_creat_simple(const char *name,
+                                 uint32_t active_level);
+
+/**
+ * @brief Dynamic creat a ll button, need set all params
+ *
+ * @param name                      Button name
+ * @param active_level              Button active level
+ * @param debounce_term             Button debounce term
+ * @param debounce_again            Button debounce again times
+ * @param long_press_term           Button long press term
+ * @param tapping_term              Button tapping term
+ */
+void ll_btn_dynamic_creat(const char *name,
+                          uint32_t active_level,
+                          uint32_t debounce_term,
+                          uint32_t debounce_again,
+                          uint32_t long_press_term,
+                          uint32_t tapping_term);
+
+/**
+ * @brief Static creat a ll button, only need set button object and 2 params, other is default
+ *
+ * @param btn           Button obeject
+ * @param name          Button name
+ * @param active_level  Button active level
+ */
+void ll_btn_static_creat_simple(ll_btn_obj_t *btn,
+                                const char   *name,
+                                uint32_t      active_level);
+
+/**
+ * @brief Static creat a ll button, need set all params
+ *
+ * @param btn                       Button obeject
+ * @param name                      Button name
+ * @param active_level              Button active level
+ * @param debounce_term             Button debounce term
+ * @param debounce_again            Button debounce again times
+ * @param long_press_term           Button long press term
+ * @param tapping_term              Button tapping term
+ */
+void ll_btn_static_creat(ll_btn_obj_t *btn,
+                         const char   *name,
+                         uint32_t      active_level,
+                         uint32_t      debounce_term,
+                         uint32_t      debounce_again,
+                         uint32_t      long_press_term,
+                         uint32_t      tapping_term);
+
+/**
+ * @brief Print all created ll buttons
+ *
+ * @return None
+ */
+void ll_btn_print_list(void);
+
+/**
+ * @brief A button attach a callback funciton, this is used for button which is created by dynamic.
+ *
+ * @param idx        The index by dynamic created button
+ * @param cb_type    The callback function type in enum(ll_btn_cb_type)
+ * @param cb         Corresponding callback function
+ */
+void ll_btn_attach_by_idx(uint32_t idx, uint8_t cb_type, ll_btn_ops_cb cb);
+
+/**
+ * @brief A button attach a callback funciton, this is used for button which is created by dynamic.
+ *
+ * @param idx        The index by dynamic created button
+ * @param cb_type    The callback function type in enum(ll_btn_cb_type)
+ */
+void ll_btn_detach_by_idx(uint32_t idx, uint8_t cb_type);
+
+/**
+ * @brief A button attach a callback funciton, this is used for button which is created by static.
+ *
+ * @param btn        Button object
+ * @param cb_type    The callback function type in enum(ll_btn_cb_type)
+ * @param cb         Corresponding callback function
+ */
+void ll_btn_attach_by_obj(ll_btn_obj_t *btn, uint8_t cb_type, ll_btn_ops_cb cb);
+
+/**
+ * @brief A button detach a callback funciton, this is used for button which is created by static.
+ *
+ * @param btn        Button object
+ * @param cb_type    The callback function type in enum(ll_btn_cb_type)
+ */
+void ll_btn_detach_by_obj(ll_btn_obj_t *btn, uint8_t cb_type);
+
+/**
+ * @brief This is ll button core, need call in a endless loop, and need a millisecond tick.
+ *
+ * @param curr_tick      A global tick, unit need millisecond
+ */
+void ll_btn_tick(uint32_t curr_tick);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
